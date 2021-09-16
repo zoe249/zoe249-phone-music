9b9c9ced2072103fea43a17aa0133be047958e74
王富贵 <1421759951@qq.com>
2021-09-16 10:41:30 +0800

    错误

--- src/views/Live/live.vue
+++ src/views/Live/live.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="wrapper">
-    <b-scroll-x class="content">
-      <h1>
-        你那天你那天你那天你那天你那天你那天你那天你那天你那天你那天你那天你那天你那天你那天
-      </h1>
-    </b-scroll-x>
+  <div class="live">
+    <!-- 导航栏 -->
+    <live-tab :titles="['推荐', '分类']"></live-tab>
+
+    <!-- 分类横向滑动 -->
+    <div class="wrapper-cate">
+      <b-scroll-x ref="scroll" class="content">
+        <div class="content-main">
+          <div
+            class="category-item"
+            v-for="(item, index) in liveCategory"
+            :key="index"
+          >
+            
+            <span>{{ item.name }}</span>
+          </div>
+        </div>
+      </b-scroll-x>
+    </div>
   </div>
 </template>
 
@@ -16,22 +29,27 @@ import {
   liveDetailPost,
   liveHoursGet,
 } from "@/network/Get/Live";
+import LiveTab from "./children/LiveTab.vue";
 import BScrollX from "@/components/comment/better-scroll/BetterScroolX.vue";
 export default {
   data() {
     return {
       // 电台分类
       type: 2,
-
+      // 分类列表
+      liveCategory: [],
       limit: 15,
     };
   },
   created() {
     // this.liveRecomPost()
     this.liveCatePost();
-    this.liveRecoTypePost()
+    this.liveRecoTypePost();
 
     this.liveHoursGet();
+  },
+  mounted () {
+    this.liveCatePost();
   },
   methods: {
     savecookie() {
@@ -46,15 +64,17 @@ export default {
     // 电台分类
     liveCatePost() {
       liveCatePost(this.savecookie).then((res) => {
-        console.log(res);
+        this.liveCategory = res.data.categories;
+        console.log(this.liveCategory);
+        this.$refs.scroll.refresh()
       });
     },
 
     // 电台分类详情
-    liveRecoTypePost(){
-      liveRecoTypePost(this.type,this.savecookie).then(res=>{
-        console.log(res)
-      })
+    liveRecoTypePost() {
+      liveRecoTypePost(this.type, this.savecookie).then((res) => {
+        console.log(res);
+      });
     },
 
     // 24小时电台
@@ -66,17 +86,38 @@ export default {
   },
   components: {
     BScrollX,
+    LiveTab,
   },
 };
 </script>
 
 <style lang="less" scoped>
-.wrapper {
+.wrapper-cate {
   white-space: nowrap;
-  width: 150px;
+  width: 100vw;
   overflow: hidden;
+  // margin-left: 3vw;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(15px);
 }
 .content {
   display: inline-block;
+  // display: flex;
+  // flex-direction: row;
+  width: 94vw;
+  margin-left: 3vw;
 }
+.content-main {
+  display: flex;
+}
+.content-main .category-item{
+  margin-right: 1em;
+  color:#fff;
+  height: 3em;
+  line-height: 3em;
+}
+// .category-item .category-item-true img{
+//   height: 2em;
+//   width: 2em;
+// }
 </style>
